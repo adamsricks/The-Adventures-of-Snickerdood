71d56d18835c80e9ca4ba9de55fb8bd78470b259
adamsricks <adam7ricks@gmail.com>
2020-12-04 13:19:46 -0700

    Finally got drag to work properly

--- player1.py
+++ player1.py
@@ -5,17 +5,18 @@ class Player1(char.Character):
     def __init__(self, gravity):
         super().__init__(gravity)
 
-        self.maxSpeed = 4
-        self.accX = .6
-
-        # these are momentum variables, and are always added to the position of the player character
+        # speed cap for x
+        self.maxSpeed = 3
+        # constant that's added to the momentum
+        self.accX = .4
+        
         self.movementx = 0
         self.movementy = 0
 
-        # this is the positive momentum, always added or subtracted to the movement variables
-        self.momentumx = 0
-        self.momentumy = 0
+        # like gravity but for the x axis
+        self.drag = .17
 
+        # speed cap for y
         self.maxFallSpeed = 5
 
         self.direction = "r"
@@ -47,37 +48,55 @@ class Player1(char.Character):
             self.direction = 'l'
 
     def advance(self):
-        super().advance()
         self.changeDirection()
         if self.direction == "r":
             self.gunPoint = (self.rect.center[0], self.rect.center[1])
         else:
             self.gunPoint = (self.rect.center[0], self.rect.center[1])
 
+        if not self.onGround:
+            if self.movementy >= self.maxFallSpeed:
+                self.movementy = self.maxFallSpeed
+                self.rect.centery += self.movementy
+            else:
+                self.movementy += self.gravity
+                self.rect.centery += self.movementy
+        else:
+            self.movementy = 0
+
         if self.moveRight:
-            if self.movementx < 0:
-                self.movementx = 0
+            # only adds momentum if not past screen boundary
             if self.rect.right < 576:
-            
                 if self.movementx <= self.maxSpeed:
                     self.movementx += self.accX
-                self.rect.centerx += self.movementx
+                #self.rect.centerx += self.movementx
             else:
                 self.movementx = 0
                 self.rect.right = 576
-
-            
+        elif not self.moveLeft:
+            if self.movementx >= self.drag:
+                self.movementx -= self.drag
 
         if self.moveLeft:
-            if self.movementx > 0:
-                self.movementx = 0
+            # only adds momentum if not past screen boundary
             if self.rect.left > 0:
                 if self.movementx >= -self.maxSpeed:
                     self.movementx -= self.accX
-                self.rect.centerx += self.movementx
+                #self.rect.centerx += self.movementx
             else:
                 self.movementx = 0
                 self.rect.left = 0
+        elif not self.moveRight:
+            if self.movementx <= -self.drag:
+                self.movementx += self.drag
+
+        #print(f"movementx before = {self.movementx}")
+        if abs(self.movementx) < self.drag:
+            self.movementx = 0
+    
+        # actually moves the character
+        self.rect.centerx += self.movementx
+        #print(f"movementx after = {self.movementx}")
 
     def draw(self, screen):
         if self.direction == "r":
